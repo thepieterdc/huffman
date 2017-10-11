0a3c5321780a65512e19077911a7ad142d0e9df7
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-10-11 12:40:08 +0200

    Datatypes

---
+++ src/datatypes/bit.h
@@ -0,0 +1,16 @@
+/**
+ * Created by Pieter De Clercq.
+ *
+ * Project: huffman
+ */
+
+#ifndef HUFFMAN_DATATYPES_BIT_H
+#define HUFFMAN_DATATYPES_BIT_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef bool bit;
+typedef uint8_t byte;
+
+#endif /* HUFFMAN_DATATYPES_BIT_H */
---
+++ src/datatypes/string.h
@@ -0,0 +1,12 @@
+/**
+ * Created by Pieter De Clercq.
+ *
+ * Project: huffman
+ */
+
+#ifndef HUFFMAN_DATATYPES_STRING_H
+#define HUFFMAN_DATATYPES_STRING_H
+
+typedef char* string;
+
+#endif /* HUFFMAN_DATATYPES_STRING_H */
