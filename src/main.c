0c8dfcae8fbd4238e60365cc482fcbcd5626b7bc
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-10-18 00:30:04 +0200

    Done

--- src/main.c
+++ src/main.c
@@ -7,9 +7,7 @@
 #include "util/arguments.h"
 
 int main(int argc, char **argv) {
-	FILE *in = fopen("/home/pieter/Downloads/bilcketest.out", "rb");
-	
-//	FILE *in = stdin;
+	FILE *in = stdin;
 	FILE *out = stdout;
 	
 	_huffmanfunction function = argument_parse(argc, argv);
