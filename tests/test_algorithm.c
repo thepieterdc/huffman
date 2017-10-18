f4664a9fa3361420a464a32bcbafc4fd76c0de7f
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-10-18 09:44:53 +0200

    Tests

--- tests/test_algorithm.c
+++ tests/test_algorithm.c
@@ -5,23 +5,46 @@
  */
 
 #include <dirent.h>
+#include <string.h>
 #include "test_algorithm.h"
-#include "../src/util/logging.h"
 #include "test_unit.h"
+#include "../src/util/logging.h"
 
-#define TEST_ALGORITHM_TESTVECTORS "./tests/testfiles/"
+#define TEST_ALGORITHM_TESTVECTORS "./tests/testvectors/"
 
 char *test_huffman_algorithm(_huffmanfunction encoder, _huffmanfunction decoder) {
-//	DIR *testvectors = opendir(TEST_ALGORITHM_TESTVECTORS);
-//	assertThat(testvectors != NULL);
-////
-////	struct dirent *dp;
-////	while ((dp = readdir(testvectors)) != NULL) {
-//////		if(dp->d_type == DT_REG) {
-//////
-//////		}
-////	}
-//
-//	closedir(testvectors);
+	DIR *testvectors = opendir(TEST_ALGORITHM_TESTVECTORS);
+	assertThat(testvectors != NULL);
+	
+	struct dirent *dp;
+	while ((dp = readdir(testvectors)) != NULL) {
+		if (dp->d_type == DT_REG) {
+			string ext = strrchr(dp->d_name, '.');
+			if(!ext || !str_equals(ext, ".txt")) {
+				continue;
+			}
+		}
+		
+		FILE *input = fopen(dp->d_name, "rb");
+		assertThat(input != NULL);
+		
+		char *encoded_buf;
+		size_t encoded_size;
+		FILE *encoded = open_memstream(&encoded_buf, &encoded_size);
+		
+		char *decoded_buf;
+		size_t decoded_size;
+		FILE *decoded = open_memstream(&decoded_buf, &decoded_size);
+		
+		encoder(input, encoded);
+		decoder(encoded, decoded);
+		
+		//* TODO TEST decoded */
+		
+		fclose(input);
+	}
+	
+	closedir(testvectors);
+	
 	return 0;
 }
