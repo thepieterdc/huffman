7f6055b490b649ee3c149ac3620636419395cd52
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-10-20 01:28:54 +0200

    Todo bugfix

--- src/main.c
+++ src/main.c
@@ -8,6 +8,32 @@
 #include "datastructures/huffman_tree.h"
 #include "util/logging.h"
 
+huffman_node *find_tbar(huffman_node *tree, huffman_node *t) {
+	huffman_node *current_best = NULL;
+	if (tree->weight == t->weight) {
+		current_best = tree;
+	}
+	
+	if (tree->left != NULL) {
+		huffman_node *left_best = find_tbar(tree->left, t);
+		if (left_best != NULL) {
+			if (current_best == NULL || current_best->order_no < left_best->order_no) {
+				current_best = left_best;
+			}
+		}
+	}
+	
+	if (tree->right != NULL) {
+		huffman_node *right_best = find_tbar(tree->right, t);
+		if (right_best != NULL) {
+			if (current_best == NULL || current_best->order_no < right_best->order_no) {
+				current_best = right_best;
+			}
+		}
+	}
+	return current_best;
+}
+
 huffman_node *find_parent_of_node(huffman_node *tree, huffman_node *node) {
 	huffman_node *partialres = NULL;
 	if (tree->left != NULL) {
@@ -44,7 +70,7 @@ int main(int argc, char **argv) {
 	huffman_node *nyt = huffman_create_node(NULL, NULL);
 	nyt->weight = 0;
 	
-	int frequencies[256] = {0};
+	huffman_node *nodes[256];
 	
 	huffman_node *tree = nyt;
 	
@@ -54,11 +80,13 @@ int main(int argc, char **argv) {
 		
 		huffman_node *t = NULL;
 		
-		if (frequencies[z]++ != 0) {
+		if (nodes[z] != NULL) {
 			info("Already in tree.");
+			t = nodes[z];
 		} else {
 			huffman_node *parent_node = find_parent_of_node(tree, nyt);
 			huffman_node *z_node = huffman_create_leaf((byte) z, 0);
+			nodes[z] = z_node;
 			z_node->weight = 1;
 			huffman_node *o_node = huffman_create_node(nyt, z_node);
 			
@@ -74,16 +102,33 @@ int main(int argc, char **argv) {
 				continue;
 			}
 		}
-//
-//		huffman_node *tbar;
-//		while (tree != t) {
-////			tbar = find_tbar(tree, t);
-////			printf("%d\n", tbar != NULL);
-//			break;
-//		}
+		
+		huffman_node *tbar;
+		//zolang t niet de wortel is//
+		while (tree != t && t != NULL) {
+			tbar = find_tbar(tree, t);
+			//t' is niet de ouder van t
+			if (tbar->left != t && tbar->right != t) {
+				//swap t en t'//
+				huffman_node t_node = *t;
+				*t = *tbar;
+				*tbar = t_node;
+				//swap ordernummers van t en t'
+				uint_fast64_t order_t = t->order_no;
+				uint_fast64_t order_tbar = tbar->order_no;
+				t->order_no = order_tbar;
+				tbar->order_no = order_t;
+			}
+			t->weight++;
+			t = find_parent_of_node(tree, t);
+		}
+		
+		//t is wortel
+		t->weight++;
 	}
 	
 	huffman_visualise_tree(tree);
+
 
 //	//FILE *in = stdin;
 //	FILE *out = stdout;
