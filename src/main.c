bc3fb6e52fe7140c596c1d8e601ed265dcbb40d3
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-10-19 21:00:21 +0200

    Main work

--- src/main.c
+++ src/main.c
@@ -7,13 +7,15 @@
 #include "util/arguments.h"
 
 int main(int argc, char **argv) {
-	//FILE *in = fopen("tests/testvectors/graag.txt", "rb");
+	FILE *in = fopen("tests/testvectors/graag.txt", "rb");
 	
-	FILE *in = stdin;
-	FILE *out = stdout;
 	
-	_huffmanfunction function = argument_parse(argc, argv);
-	function(in, out);
+	
+//	//FILE *in = stdin;
+//	FILE *out = stdout;
+//
+//	_huffmanfunction function = argument_parse(argc, argv);
+//	function(in, out);
 	
 	return 0;
 }
