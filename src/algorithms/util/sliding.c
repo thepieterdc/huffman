24bd1d8844c257ec066392bf663c73c11bda61e6
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-11-04 16:51:52 +0100

    Working on sliding

--- src/algorithms/util/sliding.c
+++ src/algorithms/util/sliding.c
@@ -5,15 +5,10 @@
  */
 
 #include "sliding.h"
+#include "../../util/logging.h"
 
 void sliding_decrease_character(adaptive_huffman_tree *tree, byte b) {
+	fprintf(stderr, "Decreasing %c\n", b);
+	huffmantree_print(tree->tree);
 	huffman_node *node = tree->tree->leaves[b];
-	
-	if (!ret) {
-		/* z is a new character; add it to the tree. */
-		huffman_node *o = adaptive_add_character(tree, character);
-		ret = o->parent;
-	}
-	
-	return ret;
 }
