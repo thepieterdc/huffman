da1c02bb402f26f538c2fb6de9b380222fe81802
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-11-10 15:09:27 +0100

    Done

--- src/main.c
+++ src/main.c
@@ -7,8 +7,8 @@
 #include "util/arguments.h"
 
 int main(int argc, char **argv) {
-	FILE *in = fopen("tests/testvectors/bruinevos.in", "rb");
-//	FILE *in = stdin;
+//	FILE *in = fopen("tests/testvectors/bruinevos.in", "rb");
+	FILE *in = stdin;
 	FILE *out = stdout;
 
 	_huffmanfunction function = argument_parse(argc, argv);
