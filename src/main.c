bd7880288ebd833617036f777fe9e138c3d73740
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-11-15 20:20:30 +0100

    Branchd

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include "util/arguments.h"
 
 int main(int argc, char **argv) {
-	FILE *in = fopen("tests/testvectors/banaan.in", "rb");
+	FILE *in = fopen("/tmp/banaantest.enc", "rb");
 	FILE *out = stdout;
 
 	_huffmanfunction function = argument_parse(argc, argv);
