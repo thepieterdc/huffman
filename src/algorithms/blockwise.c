2394e0df2bce20c3b2526ccf5c587f226e996a78
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-11-15 18:50:39 +0100

    Stuff

---
+++ src/algorithms/blockwise.c
@@ -0,0 +1,15 @@
+/**
+ * Created by Pieter De Clercq.
+ *
+ * Project: huffman
+ */
+
+#include "blockwise.h"
+
+void huffman_blockwise_compress(FILE *input, FILE *output) {
+
+}
+
+void huffman_blockwise_decompress(FILE *input, FILE *output) {
+
+}
