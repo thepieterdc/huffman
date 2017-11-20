71aceac0903a25dde95c00cc47be89b9918288e7
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-11-20 21:08:57 +0100

    Fixes #87.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
  * Project: huffman
  */
 
+#include <time.h>
 #include "util/arguments.h"
 
 int main(int argc, char **argv) {
@@ -11,7 +12,17 @@ int main(int argc, char **argv) {
 	FILE *out = stdout;
 
 	_huffmanfunction function = argument_parse(argc, argv);
+
+#ifdef IS_DEBUG
+	clock_t tic = clock();
+#endif
+	
 	function(in, out);
+
+#ifdef IS_DEBUG
+	clock_t toc = clock();
+	fprintf(stderr, "Time elapsed: %f seconds\n", (double) (toc - tic) / CLOCKS_PER_SEC);
+#endif
 	
 	return 0;
 }
